33b14fb2570dc4875c96adc5403db3402a834064
Ihor Husak <ihor.husak@epita.fr>
2020-11-24 15:19:31 +0100

    config: Pushed the bashrc of the CRI vm

---
+++ vm_config/bash_config/aliases.bash
@@ -0,0 +1,3 @@
+alias ls="ls --color=auto"
+alias resol="~/.resol.sh"
+alias tree="tree -C"
---
+++ vm_config/bash_config/git_aliases.bash
@@ -0,0 +1,4 @@
+alias gst="git status"
+alias ga="git add"
+alias gaa="git add ."
+alias glog="git log --graph --oneline"
